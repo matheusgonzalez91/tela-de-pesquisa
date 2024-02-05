3a71a385984837b9119f8143c924382efc7af072
Matheus Gonzalez <gonzalezmatheus01@gmail.com>
2024-02-05 10:59:41 -0300

    reactive-search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LibSearchComponent } from './components/lib-search/lib-search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LibSearchComponent}
+];
